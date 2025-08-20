f74705b166007f7f58d82bbd78af1d2898e770e1
christian.pilgermann <christian.pilgermann@iu-study.org>
2025-08-20 15:48:21 +0200

    cleanup and fix tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,7 +2,6 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('/')
     cy.get('.mt-3 > .btn')
-    //.children()
     .should('contain.text','Demo-Anmeldedaten anzeigen')
   })
 })
